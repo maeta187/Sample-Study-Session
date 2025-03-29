3fc74ab85273a20f9045dc2e56182ba1667aca45
maeta187 <nexus18789@gmail.com>
2025-03-29 16:51:14 +0900

    🏷️  : 型PrefectureとFormDataを追加

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export type Prefecture = {
+	code: number
+	name: string
+}
+
+export type FormData = {
+	lastName: string
+	firstName: string
+	email: string
+	sex: string
+	prefecture: string
+	message: string
+}
