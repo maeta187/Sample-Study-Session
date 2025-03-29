ad78cfa5efb5e1e666cb9e45724d084e0ffc0d18
maeta187 <nexus18789@gmail.com>
2025-03-29 16:52:11 +0900

    ✨ : フォームコンポーネントを追加し、状態管理とサブミット機能を実装

---
+++ src/app/form/_components/Form.tsx
@@ -0,0 +1,62 @@
+'use client'
+import { InputForm } from '@/app/form/_components/InputForm'
+import type { FormData, Prefecture } from '@/types'
+import { useState } from 'react'
+
+type FormProps = {
+	prefectures: Prefecture[]
+}
+
+export const Form = ({ prefectures }: FormProps) => {
+	// e.target.valueがString型であるため、全てString型
+	const [formData, setFormData] = useState<FormData>({
+		lastName: '',
+		firstName: '',
+		email: '',
+		sex: '',
+		prefecture: '',
+		message: ''
+	})
+
+	// pendingの状態管理
+	const [pending, setPending] = useState(false)
+
+	// formDataStateの更新処理
+	const handleChange = (
+		e: React.ChangeEvent<
+			HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+		>
+	) => {
+		const { name, value } = e.target
+		setFormData((prev) => ({
+			...prev,
+			[name]: value
+		}))
+	}
+
+	// バリデーション処理
+
+	// サブミット関数
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault()
+		setPending(true)
+		setTimeout(() => {
+			console.log('Form submitted:', formData)
+			setPending(false)
+		}, 3000)
+	}
+	return (
+		<div className='mx-auto w-2xl rounded-lg bg-white p-6 shadow-2xl'>
+			<h2 className='mb-6 text-center text-2xl font-bold text-gray-800'>
+				コンタクトフォーム
+			</h2>
+			<InputForm
+				formData={formData}
+				prefectures={prefectures}
+				pending={pending}
+				handleChange={handleChange}
+				handleSubmit={handleSubmit}
+			/>
+		</div>
+	)
+}
