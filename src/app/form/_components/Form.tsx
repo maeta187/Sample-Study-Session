2591d97f81f3e4af23d74d9717655e7c80000153
maeta187 <nexus18789@gmail.com>
2025-03-31 08:59:10 +0900

    ✨ : react-hook-formを使用してフォームの状態管理

--- src/app/form/_components/Form.tsx
+++ src/app/form/_components/Form.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { InputForm } from '@/app/form/_components/InputForm'
 import type { FormData, Prefecture } from '@/types'
-import { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
 type FormProps = {
 	prefectures: Prefecture[]
@@ -17,24 +17,14 @@ const defaultValues: FormData = {
 }
 
 export const Form = ({ prefectures }: FormProps) => {
-	// e.target.valueがString型であるため、全てString型
-	const [formData, setFormData] = useState<FormData>(initialFormData)
-
-	// pendingの状態管理
-	const [pending, setPending] = useState(false)
-
-	// formDataStateの更新処理
-	const handleChange = (
-		e: React.ChangeEvent<
-			HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
-		>
-	) => {
-		const { name, value } = e.target
-		setFormData((prev) => ({
-			...prev,
-			[name]: value
-		}))
-	}
+	const {
+		register,
+		handleSubmit,
+		reset,
+		formState: { isSubmitting }
+	} = useForm<FormData>({
+		defaultValues
+	})
 
 	// バリデーション処理
 
