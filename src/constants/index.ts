c116ac5621c9dff41359a14b6eec2242f14fa49e
maeta187 <nexus18789@gmail.com>
2025-03-31 09:02:34 +0900

    ✨ GENDER定数を追加し、性別の選択肢を定数化

---
+++ src/constants/index.ts
@@ -0,0 +1,15 @@
+type Gender = {
+	MEN: 'men'
+	WOMEN: 'women'
+	OTHER: 'other'
+}
+
+/** 性別 */
+export const GENDER: Readonly<Gender> = {
+	/** 男性 */
+	MEN: 'men',
+	/** 女性 */
+	WOMEN: 'women',
+	/** その他 */
+	OTHER: 'other'
+}
